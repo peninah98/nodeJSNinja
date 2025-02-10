7c050b3ed057073cc90f0ad88da241888cb7b79b
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 14:48:58 +0200

    nodejs core modules

--- builtInModules.js
+++ builtInModules.js
@@ -1,4 +1,6 @@
 // Check the current operating system the app is running on
 
 const os = require("os")
-console.log(os)
+// console.log(os)
+
+console.log(os.platform(), os.release(), os.homedir, os.hostname)
