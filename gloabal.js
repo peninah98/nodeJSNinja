30da4fb4abad0432c047ca91fbf9ab6f8c2ecf61
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 14:31:12 +0200

    check directory and file name

--- gloabal.js
+++ gloabal.js
@@ -1,8 +1,22 @@
 // global object
 
-console.log(global)
+// console.log(global)
 
 global.setTimeout(() => {
   console.log("3 seconds passed")
   console.log("This is the time out")
 }, 3000)
+
+setTimeout(() => {
+  console.log("3 seconds passed")
+  console.log("This is the time out")
+  clearInterval(int)
+}, 3000)
+
+const int = setInterval(() => {
+  console.log("In the interval")
+}, 1000)
+
+// checking current directory and file names
+console.log("Directory name", __dirname)
+console.log("Directory name", __filename)
