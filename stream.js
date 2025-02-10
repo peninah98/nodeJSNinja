ce82c0664ea44636cb1a9ed2cc7212c91633c8b0
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 16:24:59 +0200

    feat: create stream of large data

--- stream.js
+++ stream.js
@@ -3,9 +3,17 @@ const fs = require("fs")
 const readStream = fs.createReadStream("./documents/blogStream.txt", {
   encoding: "utf8",
 })
-readStream.on("data", (chunkofData) => {
-  console.log(
-    "**************************************** NEW CHUNK OF DATA ***************************************"
-  )
-  console.log(`Received ${chunkofData.length} bytes of data.`, chunkofData)
-})
+const writeStream = fs.createWriteStream("./documents/blogStreamWrite.txt")
+// readStream.on("data", (chunkofData) => {
+//   console.log(
+//     "**************************************** NEW CHUNK OF DATA ***************************************"
+//   )
+//   console.log(`Received ${chunkofData.length} bytes of data.`, chunkofData)
+//   writeStream.write("\nNew chunk of data\n")
+//   writeStream.write(chunkofData)
+// })
+
+
+//pipping
+
+readStream.pipe(writeStream)
