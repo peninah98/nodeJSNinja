275e5fb3ba06871c38033d43dcddfe9c9aa48efc
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 14:43:17 +0200

    feat: create mu own modules and export

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
-const names = require("./people")
-console.log(names)
-console.log(names.age[1])
+// const names = require("./people")
+// console.log(names)
+// console.log(names.age[1])
+
+
+const {people} = require("./people")
+console.log(people)
--- people.js
+++ people.js
@@ -1,5 +1,4 @@
  const people =["Penine", "Noella", "Mare", "Marthine"]
 const age = [1,343,23,23]
 
-console.log(people)
 module.exports = {people, age}
