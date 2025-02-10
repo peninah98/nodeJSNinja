3e662dcec2a291ad4a8c15fd3d800b8f80a3f8e0
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 14:40:41 +0200

    feat:create the export

---
+++ modules.js
@@ -0,0 +1,3 @@
+const names = require("./people")
+console.log(names)
+console.log(names.age[1])
---
+++ people.js
@@ -0,0 +1,5 @@
+ const people =["Penine", "Noella", "Mare", "Marthine"]
+const age = [1,343,23,23]
+
+console.log(people)
+module.exports = {people, age}
