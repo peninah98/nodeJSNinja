f83cee8e55c6a243b1f1b7e8fb61bcdb02a41c3c
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 15:11:01 +0200

    feat: stringfy returned buffer data:

--- files.js
+++ files.js
@@ -5,6 +5,10 @@ fs.readFile("./documents/blogs.txt", (err, data) => {
   if (err) {
     console.log(err)
   } else {
+    // these will return the buffer as a string in the console
+    // buffer is the type of data that will be returned from the server to the client side of the process that generated this file to make sure that the data is not corrupted and that the server properly handles the data appropriately it has to be stringified.
+    console.log(data)
+    //stringfy the data
     console.log(data.toString())
   }
 })
