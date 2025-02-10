59e62cc8f274a392661b600ef6febc8c0800e966
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 15:21:40 +0200

    feat: write the file

---
+++ writeFile.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+const textfile = `Detecting the platform
+React Native Module detects the platform in which the app is running and uses this option when only the small parts of the codes are platform-specific.
+
+Before we dive into platform properties let’s start with the most used properties that are used to check the platform. It returns the string value representing the current OS. it has different values. In TypeScript is an enum of three values enum(’android’ | ‘ios’, | ’ web’)`
+
+fs.writeFile("./documents/blog2.txt", textfile, () => {
+  console.log("File saved successfully")
+})
