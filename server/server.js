545ca5ee6b0a460a3ab84d16909c5b53a278574f
Peninah98 <peninangizwenayo@gmail.com>
2025-02-11 14:25:26 +0200

    feat: create header

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const server = http.createServer((req, res) => {
   console.log("request made")
   console.log(req.url, req.method, req.headers)
 
-  res.setHeader('Content-Type', 'text/plain')
-  res.write("Hello Dady!")
+  res.setHeader("Content-Type", "text/html")
+  res.write(`<head> <link rel="stylesheet" href="#"></head>`)
+  res.write(`<h1>Appreciations to my dearest parents</h1>`)
+  res.write(`<emp>Hello Dady!</emp>`)
+  res.write(`<p>Hello Dady!</p>`)
+  res.write(
+    `<p>Hello Mom I really love my dear parents you did alot for me!</p>`
+  )
   res.end()
-
 })
 
 server.listen(3000, "localhost", () => {
