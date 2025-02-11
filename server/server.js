debe0711e04912ffd068ad804fc9245586425d20
Peninah98 <peninangizwenayo@gmail.com>
2025-02-11 14:20:37 +0200

    feat: create header

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const server = http.createServer((req, res) => {
   console.log("request made")
   console.log(req.url, req.method, req.headers)
 
+  res.setHeader('Content-Type', 'text/plain')
+  res.write("Hello Dady!")
+  res.end()
+
 })
 
 server.listen(3000, "localhost", () => {
