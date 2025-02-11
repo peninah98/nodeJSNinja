6e6c5dd6847d29b92fb9996e0d66aecdec46a27b
Peninah98 <peninangizwenayo@gmail.com>
2025-02-11 16:54:13 +0200

    feat:create the server file

--- server/server.js
+++ server/server.js
@@ -3,12 +3,25 @@ const fs = require("fs")
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html")
-  fs.readFile("server/views/index.html", (err, data) => {
+  let path = "server/views"
+  switch (req.url) {
+    case "/":
+      path += "index.html"
+      break
+    case "/about":
+      path += "about.html"
+      break
+    default:
+      path += "404.html"
+      break
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log("Cannot read the file", err)
       res.end()
     } else {
-       res.end(data)
+      res.end(data)
     }
   })
 })
